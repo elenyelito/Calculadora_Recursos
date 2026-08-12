16966b9be7c680056ba8e461471e9af49101351e
Ángel Manuel López Castillo <ngel.lopez1@catolica.edu.sv>
2026-08-12 17:40:49 -0600

    Nivel de acceso

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Security.AccessControl;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("==== CALCULADORA DE RECURSOS ====");
+    /*  Console.WriteLine("==== CALCULADORA DE RECURSOS ====");
 
         Console.Write("Digite la cantidad de trabajadores: ");
         int trabajadores = int.Parse(Console.ReadLine()!);
@@ -19,5 +20,20 @@ class Program
         Console.WriteLine($"Horas trabajadas: {horas}");
         Console.WriteLine($"Consumo por hora: {consumoHora}");
         Console.WriteLine($"Consumo Total:  {consumoTotal}");
+    */
+
+        Console.WriteLine("==== NIVEL DE ACCESO ====");
+
+        Console.Write("Digite la edad del operador: ");
+        int edad = int.Parse(Console.ReadLine()!);
+
+        if (edad >= 18)
+        {
+            Console.WriteLine("Acceso permitido");
+        }
+        else
+        {
+            Console.WriteLine("Acceso restringido");
+        }
     }
 }
